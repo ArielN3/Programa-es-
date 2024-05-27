1805cb5c2dd8f34edbadc0b13f8cf1a08906e643
ArielN3 <arielsouzasilva8@gmail.com>
2024-05-27 20:14:26 -0300

    Add files via upload

---
+++ ariel.php
@@ -0,0 +1,107 @@
+<?php
+define("INFERIOR",1);
+define("SUPERIOR",100);
+
+$numero = rand(INFERIOR, SUPERIOR);
+echo "Meu numero da sorte: <br>";
+echo $numero."<br>";
+?>
+<?php
+$x = 5; //Ambito global
+
+function meuTeste () {
+    echo "<p>A variavel x dentro da função é: $x</p><br>";
+    }
+    //Ambito local
+         meuTeste ();
+    echo "<p>A variavel x dentro da função é: $x</p><br>";
+   
+?>
+
+<?php
+$x = 5;
+$y = 10;
+function numero(){
+    global $x, $y;
+    $y= $x + $y;
+    }
+numero();
+echo $y;
+?>
+
+<?php
+//ambito estatico
+function teste(){
+        static $x = 0;
+        echo $x;
+        $x++;
+    }
+echo "<br>";
+echo "<br>";
+teste();
+echo "<br>";
+teste();
+echo "<br>";
+teste();
+
+?>
+<?php
+echo "<br>Geovane da Costa";
+print "<br>Geovane da Costa<br><br>";
+?>
+<?php
+$x = 5;
+var_dump($x);
+echo "<br>";
+?>
+<?php
+$x = 'Minha string';
+$y = "Minha string 2!";
+var_dump($x);
+echo "<br>";
+var_dump($y);
+echo "<br>";
+?>
+<?php
+$x = 3.14;
+var_dump($x);
+echo "<br>";
+?>
+<?php
+$x = true;
+var_dump($x);
+echo "<br>";
+?>
+<?php
+$cars =array("BMW","BYD","AUDI");
+var_dump($cars);
+echo "<br>";
+?>
+
+<?PHP 
+$octeto = 172;
+$mascara = 248;
+$valor = $octeto & $mascara;
+echo "<br> Aplicando alteração AND entre a máscara e octeto com valor $valor";
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+?> 
+<?PHP
+$valor = 10;
+if($valor == 15){
+		echo "<br> Valor é 15";
+}
+elseif ($valor >= 10){
+echo "valor é maior ou igual a 10";
+}
+else {
+	echo"valor é menor que 10";
+}
+
+?>
+<?PHP
+echo"<br>";
+$v = "Fabio";
+$nome = $v."dos reis";
+	echo $nome;
+
+?>	
